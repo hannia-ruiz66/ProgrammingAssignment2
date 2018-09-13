d6913788bdabf597220ff1911cb02f16fd2879b2
hannia-ruiz66 <43188227+hannia-ruiz66@users.noreply.github.com>
2018-09-13 00:10:20 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  if (!is.null(i)) {                  ## get it from the cache and skips the computation.
+    message("getting cached data")
+    return(i)                         ## return old result(i) directly 
+  }
+  data <- x$get()          ## otherwise, get the uncalculated matrix
+  i <- solve(data, ...)    ## sets the value of the inverse in the cache via the setinv function.
+  x$setinverse(i)          ## reassign inverse matrix 
+  i                        ## print the inverse matrix
 }
+        
+## Testing: To test out these functions. I wrote a function called test(), which takes in any invertible matrix, 
+##calculates its inverse twice using the above functions, and prints out the times it takes for both runs. 
+##The first run should take longer than the second run because it actually calculates the inverse while the second run only does a look-up from the cache.
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+test = function(mat){
+  ## @mat: an invertible matrix
+  
+  temp = makeCacheMatrix(mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
 }
